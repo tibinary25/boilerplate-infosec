ca6a54c88afff0c4c52cd541d91706244b9be6c5
Your Name <your_email@example.com>
2025-11-18 21:59:09 +0700

    Add helmet.hsts() middleware for HTTPS enforcement

--- myApp.js
+++ myApp.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
+var ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 module.exports = app;
